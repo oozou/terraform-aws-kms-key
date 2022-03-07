a5db9e118ad88bd4e8e8c84b8d121d8ea78dc62b
artpasut <pasut67@gmail.com>
2022-03-07 12:39:45 +0700

    [output.tf] add description

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "key_arn" {
-  value = join("", aws_kms_key.this.*.arn)
+  description = "KMS key arn"
+  value       = join("", aws_kms_key.this.*.arn)
 }
 
 output "key_id" {
-  value = join("", aws_kms_key.this.*.key_id)
+  description = "KMS key id"
+  value       = join("", aws_kms_key.this.*.key_id)
 }
